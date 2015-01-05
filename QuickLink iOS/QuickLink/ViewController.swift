fb2af3007eb47528c9208d2b944a7abcc971c64f
Bodecker DellaMaria <bojdell@gmail.com>
2015-01-04 23:43:50 -0600

    added iOS app base

---
+++ QuickLink iOS/QuickLink/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickLink
+//
+//  Created by Bodecker DellaMaria on 1/4/15.
+//  Copyright (c) 2015 Bodecker DellaMaria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
